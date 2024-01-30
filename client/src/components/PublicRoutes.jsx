83c23c0a3bca4b9f37aeb912adcd72560268fc09
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-01-30 23:22:39 +0530

    publicRoutes

--- client/src/components/PublicRoutes.jsx
+++ client/src/components/PublicRoutes.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 
 function PublicRoutes(props) {
-    
+    if (localStorage.getItem('token')) {
+        return <Navigate to="/" />
+    }
+    else {
+        return props.children
+    }
 }
 
 export default PublicRoutes
