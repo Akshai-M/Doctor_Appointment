016a9151b7dfcc69642a6c3218c152890f20a169
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-04-26 12:11:32 +0530

    userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -81,7 +81,13 @@ const getUser = async (req, res) => {
     try {
         const user = await User.findById(req.userId)
 
-        
+        if (!user) {
+            return res.status(200).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
     }
     catch (error) {
         console.log(error);
