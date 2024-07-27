3ba5c0d4cc73e737a0ef4e75c2da1cf5440df0ec
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-07-27 23:57:31 +0530

    usermodel

---
+++ server/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isDoctor: {
+        type: Boolean,
+        default: false
+    },
+    seenNotifications: {
+        type: Array,
+        default: []
+    },
+    unseenNotifications: {
+        type: Array,
+        default: []
+    }
+}, { timestamps: true })
+
+export const User = mongoose.model('User', userSchema)
