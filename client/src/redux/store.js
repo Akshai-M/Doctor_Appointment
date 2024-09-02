b5114c5987052de9eb1b536c220ef1a3d75bfc96
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-09-02 23:14:21 +0530

    modified

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,5 @@
-
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
 import { alertsSlice } from '../redux/alertsSlice'
 import { userSlice } from './userSlice'
 
@@ -7,6 +8,8 @@ export const rootReducer = combineReducers({
     user: userSlice.reducer
 })
 
-
+const store = configureStore({
+    reducer: rootReducer
+})
 
 export default store
